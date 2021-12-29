c55aece28829bdac755fe267db39e23e9d8ac399
Sean <seanhoyeonjin@gmail.com>
2021-12-29 18:40:02 -0500

    setting up the calendar UI/UX

--- frontend/src/components/DateCalendar.js
+++ frontend/src/components/DateCalendar.js
@@ -2,17 +2,16 @@ import "./DateCalendar.css";
 import Calendar from 'react-calendar';
 import { useState } from "react";
 
-function DateCalendar() {
+function DateCalendar(props) {
 
-  const [date, setDate] = useState(new Date());
+  const { date, setDate } = props;
 
   return (
     <div>
-      <h1>React Calendar</h1>
-      <div>
+      <div className="calendar-container">
         <Calendar onChange={setDate} value={date} />
       </div>
-      <p>
+      <p className="text-center">
         <span>Selected Date:</span>
         {date.toDateString()}
       </p>
--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import NavBar from "./NavBar";
 import DateCalendar from "./DateCalendar";
-import './MainPage.css'
+import "./MainPage.css";
 
 function MainPage() {
+  const [date, setDate] = useState(new Date());
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
   return (
     <div className="main-container">
       <NavBar />
-      <DateCalendar />
+      <div className="calendar-container">
+        <div className="selected-day">
+        <div>{months[date.getMonth()]}</div>
+          <div>{date.getDate()}</div>
+          <div>{weekdays[date.getDay()]}</div>
+          <div>
+            <button>View Tasks</button>
+          </div>
+        </div>
+        <DateCalendar date={date} setDate={setDate} />
+      </div>
     </div>
   );
 }
