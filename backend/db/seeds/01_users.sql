b012229f2e76a1fec79a8e2b47c7adf4be0d0541
Sean <seanhoyeonjin@gmail.com>
2021-12-30 19:28:39 -0500

    seeded the database, successful

---
+++ backend/db/seeds/01_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (first_name, last_name, email, password)
+VALUES ('Sean', 'Jin', 'sean@email.com', 'test')
---
+++ backend/db/seeds/02_tasks.sql
@@ -0,0 +1,3 @@
+INSERT INTO tasks (user_id, name, description, type, date, status, high_priority)
+VALUES (1, 'Drink Coffee', 'to wake up', 'food', '2021-12-29', 'completed', false),
+(1, 'Do Laundry', 'no comment', 'house', '2021-12-29', 'in-progress', true);
