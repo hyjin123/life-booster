5491d93bdd4f2f7a89c323be602ca52356cfbba3
Sean <seanhoyeonjin@gmail.com>
2021-12-30 21:36:13 -0500

    able to access form data

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,14 +1,24 @@
 import "./NavBar.css";
 import { Nav } from "react-bootstrap";
+import { FaHome } from 'react-icons/fa';
 
 function NavBar() {
   return (
     <div className="position-relative">
       <Nav defaultActiveKey="/home" className="flex-column nav-container">
-        <Nav.Link className="nav" href="/">Life Booster</Nav.Link>
-        <Nav.Link className="nav" href="/home">Home</Nav.Link>
-        <Nav.Link className="nav" href="/register">Register</Nav.Link>
-        <Nav.Link className="nav" href="/login">Login</Nav.Link>
+        <Nav.Link className="nav" href="/">
+          Life Booster
+        </Nav.Link>
+        <Nav.Link className="nav" href="/home">
+          <FaHome />
+          Home
+        </Nav.Link>
+        <Nav.Link className="nav" href="/register">
+          Register
+        </Nav.Link>
+        <Nav.Link className="nav" href="/login">
+          Login
+        </Nav.Link>
         <Nav.Link className="nav" eventKey="disabled" disabled>
           Disabled
         </Nav.Link>
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -3,27 +3,39 @@ import { Form, Button } from "react-bootstrap";
 import './Register.css'
 
 function Register() {
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // save the form data to each variable
+    const formData = new FormData(event.currentTarget);
+    const firstName = formData.get('first-name');
+    const lastName = formData.get('last-name');
+    const email = formData.get('email');
+    const password = formData.get('password');
+    console.log(email);
+  };
+
   return (
     <div className="register-container">
       <NavBar />
       <div className="form-container">
-        <Form>
+        <Form onSubmit={onSubmit}>
           <Form.Group className="mb-3">
             <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter First Name" />
+            <Form.Control type="text" name="first-name" placeholder="Enter First Name" />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter Last Name" />
+            <Form.Control type="text" name="last-name" placeholder="Enter Last Name" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" name="email" placeholder="Enter email" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" name="password" placeholder="Password" />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
