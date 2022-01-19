49d6fa7eb5a570cf14eefd6d8289a0f3669478f9
Sean <seanhoyeonjin@gmail.com>
2022-01-19 17:26:22 -0500

    created a react boostrap popup for adding new tasks

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,7 +1,21 @@
-import React from "react";
-import { Tabs, Sonnet, Tab } from "react-bootstrap";
+import { React, useState } from "react";
+import { Tabs, Tab, Modal, Button, Form } from "react-bootstrap";
+import "./List.css"
 
 export default function List(props) {
+  // state for the add task popup
+  const [show, setShow] = useState(false);
+
+  // handling the open and close of the add task pop up
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const handleShow = () => {
+    setShow(true);
+  };
+
+  // props
   const { date, months, weekdays, setListOn } = props;
 
   // when a user clicks the back button, it renders and brings back the calendar
@@ -18,6 +32,9 @@ export default function List(props) {
       <h3>
         <button onClick={handleClick}>Go Back to the Calendar</button>
       </h3>
+      <h3>
+        <button onClick={handleShow}>Add a Task</button>
+      </h3>
       <Tabs
         defaultActiveKey="home"
         id="uncontrolled-tab-example"
@@ -28,6 +45,38 @@ export default function List(props) {
         <Tab eventKey="progress" title="In-progress"></Tab>
         <Tab eventKey="completed" title="Completed"></Tab>
       </Tabs>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add a Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Enter your task below:</Modal.Body>
+        <Form className="add-task-form">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+        </Form>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
