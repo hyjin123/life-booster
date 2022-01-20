dc832d2658e2416b4c9a9c45362b84886a09d96a
Sean <seanhoyeonjin@gmail.com>
2022-01-20 16:51:24 -0500

    started working on the table for all displaying all tasks

--- frontend/src/components/AllTasks.js
+++ frontend/src/components/AllTasks.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
+import "./AllTasks.css"
 
 export default function AllTasks(props) {
   return (
-  <div>
-    <h1>this is all tasks component</h1>
-  </div>
-  )
+    <div className="table-container">
+      <div className="table-header">
+        <div>Type</div>
+        <div>Name</div>
+        <div>Details</div>
+        <div>Status</div>
+        <div>Edit</div>
+        <div>Delete</div>
+      </div>
+      <div className="table-row">
+        <div>hello</div>
+      </div>
+    </div>
+  );
 }
+
+// icon/type
+// name
+// details
+// status
+// delete button
+// completed button
+// in progress button
+// not completed button
+// edit button
